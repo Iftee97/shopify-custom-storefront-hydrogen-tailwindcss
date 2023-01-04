cdf6a1d47b6a7be5ed9e09bc560c0f9ef6d2e0ad
Iftee97 <tawsifhaque97@gmail.com>
2023-01-04 15:52:29 +0600

    refactors

--- src/routes/add-product.server.jsx
+++ src/routes/add-product.server.jsx
@@ -3,28 +3,25 @@ import Layout from '../components/Layout.server'
 import ProductDetails from '../components/ProductDetails.client'
 
 export default function AddProduct() {
-  const stringifiedProduct = JSON.stringify({
-    "product": {
-      "title": "Very cool product 2",
-      "body_html": "Html body for product",
-      "vendor": "The biggest vendor",
-      "product_type": "Mini product",
-      "tags": [
-        "winter",
-        "cold"
-      ]
-    }
-  })
-
   const product = fetchSync('https://another-store0.myshopify.com/admin/api/2022-10/products.json', {
     method: 'post',
     headers: {
       'Content-Type': 'application/json',
       'X-Shopify-Access-Token': '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'
     },
-    body: stringifiedProduct
+    body: JSON.stringify({
+      "product": {
+        "title": "Very cool product 2",
+        "body_html": "Html body for product",
+        "vendor": "The biggest vendor",
+        "product_type": "Mini product",
+        "tags": [
+          "winter",
+          "cold"
+        ]
+      }
+    })
   }).json()
-
   console.log('product', product)
 
   return (
